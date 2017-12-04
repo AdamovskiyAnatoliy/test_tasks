bf385fcc0a2273fbd72d6d7b37947740020b6234
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-12-04 12:09:50 +0200

    small fix

--- task_3_sql/filling_table.sql
+++ task_3_sql/filling_table.sql
@@ -5,10 +5,10 @@ INSERT INTO general(email, name, subscription_date)
 VALUES('b@gamail.com', 'Банан', '2001-12-16 20:38:40');
 
 INSERT INTO general(email, name, subscription_date)
-VALUES('c@gamail.com', 'Вишня', '2001-11-16 20:38:40');
+VALUES('c@gamail.com', 'Вишня', '2017-12-16 20:38:40');
 
 INSERT INTO general(email, name, subscription_date)
-VALUES('d@gamail.com', 'Груша', '2001-11-16 20:38:40');
+VALUES('d@gamail.com', 'Груша', '2017-12-16 20:38:40');
 
 INSERT INTO general(email, name, subscription_date)
 VALUES('e@gamail.com', 'Диня', '2001-12-16 20:38:40');
--- task_3_sql/request.sql
+++ task_3_sql/request.sql
@@ -1,3 +1,4 @@
 SELECT email, name, subscription_date 
 FROM general 
-WHERE EXTRACT(MONTH FROM subscription_date) = EXTRACT(MONTH FROM now());
+WHERE EXTRACT(MONTH FROM subscription_date) = EXTRACT(MONTH FROM now()) AND
+EXTRACT(YEAR FROM subscription_date) = EXTRACT(YEAR FROM now());
